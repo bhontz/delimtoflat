cd1dc70b33312a953c11e75d227e01ffa699219b
brad <bhontz@paiwhq.com>
2014-05-23 16:07:53 -0700

    20140523 v.02

---
+++ DelimToFlatClass.py
@@ -0,0 +1,272 @@
+"""
+    DelimToFlat.py
+    
+    A class that supports converting delimited files to flat files.
+    
+    Class includes the following methods:
+    
+    CreateLayout() - Determines the maximum field widths of each field within the delimited file, producing a CSV file of the field names and widths
+    
+    Flatten() - Takes the layout (presumably modified) file created by CreateLayout() and uses the layout to create a flat file from the delimited file.
+    "Presumably modified" means that the layout.csv file can be modified in terms of changing the field ordering, removing fields, designating field widths
+    as V which means the data from these fields will be written into a verbatim file, and adding fields(s) with field name P-A-D-D-I-N-G to force deliberate
+    spacing within the fixed file output.    
+"""
+__version__ = "$Revision: 0.02 $"
+__source__ = "$Header: \python27\DelimToFlat.py, v0.02 5/21/2014 $"
+
+import sys, unicodecsv, csv, time, codecs, unicodedata
+
+class DelimToFlat():
+    """
+        manages the FTP process and returns a list of validated files pulled
+        from the project's FTP folder
+    """
+    def __init__(self, dictParameters):
+        if dictParameters.has_key("DELIMpathfn"):
+            self.strDELIMpathfn = dictParameters["DELIMpathfn"]
+        if dictParameters.has_key("LAYOUTpathfn"):
+            self.strLAYOUTpathfn = dictParameters["LAYOUTpathfn"]
+        if dictParameters.has_key("CENDpathfn"):
+            self.strCENDpathfn = dictParameters["CENDpathfn"]
+        if dictParameters.has_key("VERBpathfn"):
+            self.strVERBpathfn = dictParameters["VERBpathfn"]
+        if dictParameters.has_key("DELIMchar"):
+            self.strDELIMchar = self.__CleanDelimChar(dictParameters["DELIMchar"])  # assumes dictionary contains brackets around delimiter like [\t]
+        if dictParameters.has_key("UIDfldname"):
+            self.strUIDfldname = dictParameters["UIDfldname"].upper()  # user needs to identify the respondent UID fieldname
+        return
+    
+    def __del__(self):
+        return
+    
+    def PrintParameters(self):
+        print self.strDELIMpathfn
+        print "(%s)" % self.strDELIMchar
+        return
+    
+    def CreateLayout(self):
+        """
+            CreateLayout() - method used to create a LAYOUT file (csv) representing the maximum field width of each field in the delimited file
+        """
+        bHasBOM, nLenBOM = self.__HasBOM(self.strDELIMpathfn)
+            
+        try:
+            fDelim = open(self.strDELIMpathfn,'rb')
+        except IOError, detail:
+            print "unable to open file %s for READ - details:%s" % (self.strDELIMpathfn, detail)
+            sys.exit(-1)
+            
+        if bHasBOM:
+            fDelim.seek(long(nLenBOM))   # move past the BOM marker if there is one
+            
+        try:
+            fControl = open(self.strLAYOUTpathfn, 'wt')
+        except IOError, detail:
+            print "unable to open file - details:%s" % detail
+            sys.exit(-1)
+        
+        reader = unicodecsv.reader(fDelim, delimiter=self.strDELIMchar, encoding='UTF-8')   
+        lstHeaderRecord = reader.next()   # capture the header record of the delimited file     
+                    
+        dictMaxFieldLns = self.__InitializeDictionary(lstHeaderRecord)
+        
+        lRec = 0L
+        for row in reader:
+            lRec += 1L
+            for i, col in enumerate(lstHeaderRecord):
+                nLen = len(row[i])
+                if dictMaxFieldLns[col] < nLen:
+                    dictMaxFieldLns[col] = nLen
+    
+        # now that dictMaxFieldLns is populated with the max field width per field, write out the control file                
+        fControl.write("FieldName,MaxFldWidth\n")  # write a header record...
+        for col in lstHeaderRecord:
+            v = dictMaxFieldLns[col]
+            fControl.write("%s,%d\n" % (col, v))
+                       
+        fControl.close()
+        fDelim.close()        
+        
+        return
+        
+    def Flatten(self):
+        """
+            Takes a two field CSV control file containing a header record.  Fields are FieldName and MaxFldWidth.  The MaxFldWidth column can contain
+            a value of V which will write the delimited file content for this field to the VERBATIM file.   A FieldName value of P-A-D-D-I-N-G will be accepted
+            and will create a blank field of the width specified.  Output file has codepage cp-1252 (aka ASCII).  If a VERBATIM file is specified and created
+            it will be created in using a UTF-8 code page with field values of UID, fieldname and verbatim field value.
+        """
+        bHasVerbatim = False
+        
+        try:
+            fControl = open(self.strLAYOUTpathfn, 'rt')
+        except IOError, detail:
+            print "unable to open file LAYOUT file for read: %s - details:%s" % (self.strLAYOUTpathfn, detail)
+            sys.exit(1)
+        else:
+            reader = csv.reader(fControl, delimiter=',')   # control file uses comma delimiter
+            reader.next() # skip header record
+            lstControl = list(reader)
+            for l in lstControl:
+                l[0] = str(l[0].upper())   # ucase the fieldname to facilitate matchbacks
+                if l[1] == 'V' or l[1] == 'v':   # we know we will need the VERBpathfn
+                    bHasVerbatim = True
+            del reader
+            fControl.close()
+            del fControl
+     
+            try:
+                fout = codecs.open(self.strCENDpathfn, encoding='cp1252', mode='w', errors='ignore')
+            except IOError, detail:
+                print "unable to open file CEND OUTPUT FILE:%s for WRITE - details:%s" % (self.strCENDpathfn, detail)
+                sys.exit(1)
+            
+            if bHasVerbatim:
+                try:
+                    fFileSet = codecs.open(self.strVERBpathfn, encoding='UTF-8', mode='w', errors='ignore')
+                except IOError, detail:
+                    print "unable to open VERBATIM OUTPUT file %s for WRITE - details:%s" % (self.strVERBpathfn, detail)
+                    sys.exit(1)
+    
+            bHasBOM, nLenBOM = self.__HasBOM(self.strDELIMpathfn)
+    
+            try:
+                fDelim = open(self.strDELIMpathfn, 'rb')
+            except IOError, detail:
+                print "unable to open DELIMITED file %s for READ - details:%s" % (self.strDELIMpathfn, detail)
+                sys.exit(1)
+            else:
+                
+                if bHasBOM:   # push past the BOM marker if one is found
+                    fDelim.seek(long(nLenBOM))
+        
+                reader = unicodecsv.reader(fDelim, delimiter=self.strDELIMchar, encoding='UTF-8')
+                lstHeaderRecord = reader.next()
+                lstHeaderRecord = list(str(l.upper()) for l in lstHeaderRecord)
+                
+                # build up a dictionary with keys of field labels and values of the field column ordering (0,1,2...)
+                nMaxFldNameWidth = 0
+                dictHeaderRecord = {}
+                for i, col in enumerate(lstHeaderRecord):
+                    if len(col) > nMaxFldNameWidth:
+                        nMaxFldNameWidth = len(col)
+                    dictHeaderRecord[col] = i
+                    
+                nMaxFldNameWidth = self.__VERBFldNameWidth(nMaxFldNameWidth)
+                
+                nUID = lstHeaderRecord.index(self.strUIDfldname)  # column of the record UID
+                lRec = 0L                
+                for row in reader:
+                    for r in lstControl:
+                        if r[0] == "P-A-D-D-I-N-G":   # this field can be inserted into the layout to insert blank spaces
+                            nWidth = int(r[1])
+                            fout.write("%s" % ''.center(nWidth))
+                        else:
+                            if r[0] == self.strUIDfldname:
+                                nUIDWidth = int(r[1])   # capture this field width in for the VERBpathfn layout
+                                
+                            nCol = dictHeaderRecord[r[0]]
+                            if r[1] == 'V' or r[1] == 'v':
+                                strVerbatim = row[nCol].rstrip().lstrip()
+                                if strVerbatim != "":
+                                    fFileSet.write("%-*.*s%-*.*s%s\n" % (nUIDWidth,  nUIDWidth, row[nUID], nMaxFldNameWidth, nMaxFldNameWidth, r[0], strVerbatim))
+                            else:
+                                nWidth = int(r[1])
+                                strValue = ""
+                                if dictHeaderRecord.has_key(r[0]):
+                                    strValue = self.__strip_accents(unicode(row[nCol]))
+                                fout.write("%-*.*s" % (nWidth,nWidth,strValue))
+                    fout.write('\n')
+                
+                fout.close()
+                if bHasVerbatim:
+                    fFileSet.close()
+                        
+        return
+        
+    def __HasBOM(self, strPathfn):
+        """
+            Looks at a file passed by pathfile name and determines if the file contains a BOM character or not (returns true/false)
+        """
+        bHasBOM = False
+        nLenBOM = 0  # can be different lengths, returns 0 if bHasBOM = False
+        
+        lstBOMAlternatives = [codecs.BOM_UTF32, codecs.BOM_UTF16, codecs.BOM_UTF8]
+    
+        try:
+            fp = open(strPathfn,'rb')
+        except IOError, detail:
+            print "unable to open file %s for READ - details:%s" % (strPathfn, detail)
+            sys.exit(-1)
+        else:
+            header = fp.read(4)  # just need the first four bytes of the file to determine if a BOM exists
+            fp.close()
+            
+            for l in lstBOMAlternatives:
+                if header.find(l) == 0:
+                    bHasBOM = True
+                    nLenBOM = len(l)
+                    break
+                
+        return bHasBOM, nLenBOM
+    
+    def __InitializeDictionary(self, lstHeader):
+        """
+            Initializes a dictionary which will contain keys of the fields in the delimited file
+            and values of the max widths of the fields within the delimited file
+        """
+        d = {}
+        for l in lstHeader:
+            if d.has_key(l):
+                print "Fatal error - file contains replicated field name:%s.  Fatal, aborting."
+                sys.exit(-1)
+            else:
+                d[l] = 0
+        return d
+    
+
+    def __strip_accents(self, s):
+        return unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore')
+        
+        
+    def __CleanDelimChar(self, s):
+        """
+            assumes strDelimiter is passed with square brackets like [|] or [\t].
+            Returns the single character that would be used by methods requiring a delimiter character
+        """
+        if s.find('[') != -1 and s.find(']') > 0:
+            s = s.replace('[', '').replace(']', '')
+            if s == '\\t':
+                s = '\t'
+        else:
+            print "Invalid delimiter specified within the parameters dictionary:%s" % s
+            s = None   # this will cause a fail somewhere down the road
+                
+        return s
+    
+    def __VERBFldNameWidth(self, nMaxWidth):
+        """
+            In an effort to keep a consistent field width between very similar projects, this project returns
+            widths of 16, 24, 32, 48, 64 or 128 dependent upon the nMaxWidth argument.  Realize this doesn't work
+            if the widths are close to the list parameters below.
+            Will return the nMaxWidth passed argument if nMaxWidth exceeds 128
+        """
+        lstLen = [16, 24, 32, 48, 64, 128]
+        for v in lstLen:
+            if v > nMaxWidth:
+                nMaxWidth = v
+                break
+            
+        return nMaxWidth  
+
+if __name__ == '__main__':
+    dictNew = {"DELIMpathfn": "/users/brad/my projects/raw/Nissan_Sales_20140520.txt", "DELIMchar": "[|]", \
+               "LAYOUTpathfn": "/users/brad/my projects/raw/Nissan_Sales_20140520_LAYOUT.csv", \
+               "CENDpathfn" : "/users/brad/my projects/raw/BradRules.dat", "UIDfldname": "SampleID", \
+               "VERBpathfn" : "/users/brad/my projects/raw/BradRulesVERB.dat"}
+    obj = DelimToFlat(dictNew)
+    ###obj.CreateLayout()
+    obj.Flatten()
+    del obj
+    
---
+++ DelimitedToFlat.py
@@ -0,0 +1,106 @@
+"""
+    DelimitedToFlat.py
+    
+    Command line procedure for converting delimited files to flat files.
+    
+    Requires a Parameters.txt file to be passed as an argument.  This file can use any filename, but the structure specified below is required.
+    
+    Additional (optional) argument:
+    -l CREATES the layout file.  You will need to use this argument when dealing with a new delimited file for the first time.
+    
+    Typically, you will modify the layout file in the following ways before running the script again to create your flat file:
+        1) Any VERBATIM field should be identified by replacing the field width with a V
+        2) You can reorder the fields any way you want to specify the layout of your flat file
+        3) You can change the field widths any way you want, though note the fields widths provided by the -l argument are the maximum width found in the data
+        4) You can remove fields you don't want in your flat file
+        5) You can add a field with name P-A-D-D-I-N-G to add a "blank space" within your flat file.  Specify the field width you want for your padding.
+        
+    Structure of the Parameters.txt file:
+        DELIMpathfn = e:/raw/myproject/some-delimited-file.txt
+        LAYOUTpathfn = e:/raw/myproject/layout-of-delimited-file.csv   # note must be CSV -- will contain two fields, file name and field width
+        CENDpathfn = e:/raw/myproject/closed-end-data.dat
+        VERBpathfn = e:/raw/myproject/verbatim-data.dat   # note you don't need to specify this argument if your layout file doesn't include a V field width
+        DELIMchar = [|]   # other options here would be for tab delimited [\t] or comma delimited [,]
+        UIDfldname = RespondentID   # you will need to edit the some-delimited-file.txt and identify the unique respondent identifier field and designated it here
+        
+   Note that the layout of the VERBATIM file will be RespondentID  FieldName  Verbatim.  The width of the RespodentID field is determined in the layout file.
+   The FieldName field with will be either 16, 24, 32, 48, 64, 128 (or wider) depending up the maximum field width of the fields in the layout file
+   The VERBATIM file is written out as UTF-8 whereas the CEND file is written as a cp-1252 (ascii) code page file
+        
+"""
+__version__ = "$Revision: 0.02 $"
+__source__ = "$Header: \python27\DelimToFlat.py, v0.02 5/21/2014 $"
+
+import os, sys, time, argparse
+from DelimToFlatClass import DelimToFlat
+
+def HandleCommandLineArguments():
+   path, fn = os.path.split(sys.argv[0])
+   if len(sys.argv) == 1:
+      sys.stdout.write("C:\ScriptsFolder>python %s -h\n\tUse the -h argument after the module name to list the arguments required." % fn)
+      sys.exit(1)
+   
+   strUsageMessage = \
+   """
+   %s converts a delimited text file to a flat file.  Will split a delimited file into CMR "closed end" and seperate "verbatim" filesets file.
+   
+   Example:
+      C:\pythonscripts\python DelimitedToFlat.py [-l] parameters.txt
+   """ % fn.upper()
+   
+   parser = argparse.ArgumentParser(description=strUsageMessage)
+   parser.add_argument('strPARAMpathfn', action="store", type=str, help="Pathfilename of the required parameter file.  See the README for instructions.")
+   parser.add_argument("-l", "--bCreateLayout", help="use -l to CREATE the layout file specified within parameters.txt", action="store_true")
+   
+   ns = parser.parse_args()
+   
+   return ns
+
+
+def ParseParameterFile(fp):
+    """
+        This method parses the tab delimited parameter file and then returns the parameters as a dictionary
+    """
+    dictParameters = {}
+    lstParams = fp.readlines()
+    for row in lstParams:
+        lstRow = row.split("=")
+        k = lstRow[0].rstrip().lstrip()
+        if not dictParameters.has_key(k):
+            dictParameters[k] = str(lstRow[1]).rstrip().lstrip()
+    fp.close()
+    del fp
+                            
+    return dictParameters
+
+if __name__ == '__main__':
+    
+    print "hello from DelimitedToFlat.py"
+    
+    argns = HandleCommandLineArguments()   # can comment this line out
+ 
+    if os.path.exists(argns.strPARAMpathfn):
+        try:
+            fpPARAM = open(argns.strPARAMpathfn, "rt")
+        except IOError, detail:
+            sys.stdout.write("Unable to open file:%s for READ. Details:%s" % (argns.strPARApathfn, detail))
+        else:
+            dictParameters = ParseParameterFile(fpPARAM)
+            fpPARAM.close()
+                        
+            sys.stdout.write("--------------------------------------------------------------\n")
+            sys.stdout.write("Start of Process: %s\n\n" % time.strftime("%H:%M:%S", time.localtime()))
+            
+            obj = DelimToFlat(dictParameters)
+            if argns.bCreateLayout:
+                obj.CreateLayout()
+            else:
+                obj.Flatten()
+                
+            del obj
+
+            sys.stdout.write("\n\nEnd of Process: %s\n" % time.strftime("%H:%M:%S", time.localtime()))
+            sys.stdout.write("-------------------------------------------------------------\n")    
+        
+    else:
+        print "Can't find parameter file pathfilename:%s.  Aborting." % argns.strPARAMpathfn
